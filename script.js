7bdc61bc0859c2f3b4fb7496961abf62137a000c
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-22 22:54:54 +0200

    Started validation.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const submitBtn = document.querySelector("#submit");
+const form = document.querySelector(".sign-up");
+const firstNameField = document.querySelector("#first-name");
+const lastNameField = document.querySelector("#last-name");
+const emailField = document.querySelector("#email");
+const passField = document.querySelector("#password");
 
+const nameRegex = /^[a-z ,.'-]+$/i;
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+submitBtn.addEventListener("click", (e) => {
+  // e.preventDefault();
+  // alert("clicked");   // it's working
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //   alert("submitting");
+  let firstName = firstNameField.value.trim();
+  let lastName = lastNameField.value.trim();
+  let email = emailField.value.trim();
+  let password = passField.value.trim();
+
+  //   alerting all the info
+  //   alert(firstName + " " + lastName + " " + email + " " + password);
+});
+
+function isValid(field, regex) {
+  return String(field).toLowerCase().match(complexRegex);
+}
