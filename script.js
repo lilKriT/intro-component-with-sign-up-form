3149ac1b4c18d1d377efbe9df79e6d4476840bae
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-23 23:15:12 +0200

    Working on validation.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const submitBtn = document.querySelector("#submit");
 const form = document.querySelector(".sign-up");
 const firstNameField = document.querySelector("#first-name");
 const lastNameField = document.querySelector("#last-name");
@@ -9,11 +8,6 @@ const nameRegex = /^[a-z ,.'-]+$/i;
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-submitBtn.addEventListener("click", (e) => {
-  // e.preventDefault();
-  // alert("clicked");   // it's working
-});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
